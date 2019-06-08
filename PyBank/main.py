61521ed9843324ac05e6d5bdd6adc6a11ed3177f
Yanhuan Huang <hyhuan0318@gmail.com>
2019-06-07 20:53:30 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -17,5 +17,21 @@ with open(budget_csv, "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
     header = next(csvreader)
+    print(header)
 
+    #the total number of months included in the dataset
+    months = []
+    for row in csvreader:
+        months.append(row[0])
+    num_of_month = len(months)
+
+    #The net total amount of "Profit/Losses" over the entire period
+    net_total = 0
+    for row in csvreader:
+        net_total += int(row[1])
+
+    #The average of the changes in "Profit/Losses" over the entire period
+    avg_change = net_total/num_of_month
+    
+    #The greatest increase in profits (date and amount) over the entire period
     
