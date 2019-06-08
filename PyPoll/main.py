7783289d42af45262d317773086fb4f7d474cfeb
Yanhuan Huang <hyhuan0318@gmail.com>
2019-06-07 22:42:32 -0400

    Update main.py

---
+++ PyPoll/main.py
@@ -0,0 +1,7 @@
+#Create a Python script that analyzes the votes and calculates each of the following:
+
+#The total number of votes cast
+#A complete list of candidates who received votes
+#The percentage of votes each candidate won
+#The total number of votes each candidate won
+#The winner of the election based on popular vote.
