877916d9091f5ba28ec8ad4ff7ffd53b402fcfc0
Yanhuan Huang <hyhuan0318@gmail.com>
2019-06-08 13:37:25 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -22,37 +22,42 @@ with open(budget_csv, "r") as csvfile:
     #The net total amount of "Profit/Losses" over the entire period
     months = []
     profit_losses = []
+
     for row in csvreader:
         months.append(row[0])
         profit_losses.append(int(row[1]))
+    
     num_of_month = len(months)
     net_total = sum(profit_losses)
-    print(profit_losses)
+    max_increase = max(profit_losses)
+    min_increase = min(profit_losses)
+
+    print(max_increase)
+    print(min_increase)
 
     #The average of the changes in "Profit/Losses" over the entire period
     changes = []
     i = 0
     while i < len(profit_losses)-1:
-        num = profit_losses[i+1]-profit_losses[1]
+        num = profit_losses[i+1]-profit_losses[i]
         changes.append(num)
         i += 1
+    print(changes)
     avg_changes = sum(changes)/len(months)
     
     #The greatest increase in profits (date and amount) over the entire period
     #The greatest decrease in losses (date and amount) over the entire period
-    with open(budget_csv, "r") as csvfile:
-        max_increase = max(profit_losses)
-        min_increase = min(profit_losses)
-        for row in csvreader:
-            if int(row[1]) == max_increase:
-                max_month = row[0]
-            if int(row[1]) == min_increase:
-                min_month = row[0]
+    #with open(budget_csv, "r") as csvfile:
+    #    for row in csvreader:
+    #        if int(row[1]) == max_increase:
+    #            max_month = row[0]
+    #        if int(row[1]) == min_increase:
+    #            min_month = row[0]
 
     print("Financial Analysis")
     print("----------------------------")
     print(f"Total Months: {num_of_month}")
     print(f"Total: ${net_total}")
     print(f"Average Change: ${avg_changes}")
-    print(f"Greatest Increase in Profits: {max_month} (${max_increase})")
-    print(f"Greatest Increase in Profits: {min_month} (${min_increase})")
+    #print(f"Greatest Increase in Profits: {max_month} (${max_increase})")
+    #print(f"Greatest Increase in Profits: {min_month} (${min_increase})")
